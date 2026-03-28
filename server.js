bf849907985fbd34949d5f77c2299666a13c6553
Himanshu Pagare <155243085+Himanshu-cyber-alt@users.noreply.github.com>
2026-03-28 16:21:25 +0530

    fix

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ const PORT = 8000;
 
 
 app.get("/",(req,res)=>{
-return res.send({message : "Hello from the Docker v2"});
+return res.send({message : "Hello from the Docker v3"});
 });
 
 app.get("/home",(req,res)=>{
     return res.send(`Hello From Docker`);
 })
 
-
+app.get("/docker",(req,res)=>{
+    return res.send("lets check again for render are you deploy properly this is form Docker")
+})
 
 
 
